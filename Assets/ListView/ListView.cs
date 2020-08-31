5774b09e5b5cceb3ab79b3fc0c03a59d90357264
wangjiangrong <903036544@qq.com>
2020-08-31 15:45:40 +0800

    calculate listview row and column, and set listview size

--- Assets/ListView/ListView.cs
+++ Assets/ListView/ListView.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
+[ExecuteInEditMode]
 [RequireComponent(typeof(GridLayoutGroup))]
-[RequireComponent(typeof(ContentSizeFitter))]
 public class ListView : MonoBehaviour
 {
     public enum ESelectType
@@ -20,20 +20,27 @@ public class ListView : MonoBehaviour
     Action<ListViewItem> m_onItemClicked;
     
     bool m_isVirtual;
+    bool m_isHorizontal;
+    int m_rowCount, m_columnCount;
+    Vector2 m_initialSize;
+    Vector2 m_padding;
+    Vector2 m_itemSize;
 
     public ESelectType selectType;
     
-    Transform m_transform;
+    RectTransform m_rectTransform;
     GridLayoutGroup m_gridLayoutGroup;
     ScrollRect m_scrollRect;
     GameObjectPool m_pool;
 
     public GameObject itemPrefab { get; set; }
     int m_itemCount;
+    float m_scrollStep;
+    bool m_isBoundsDirty = false;
 
     public int itemCount
     {
-        get => m_itemCount;
+        get => m_itemList.Count;
         set
         {
             int oldCount = m_itemList.Count;
@@ -60,11 +67,22 @@ public class ListView : MonoBehaviour
     
     void Awake()
     {
-        m_transform = transform;
+        m_rectTransform = GetComponent<RectTransform>();
         m_gridLayoutGroup = GetComponent<GridLayoutGroup>();
         
         m_itemList = new List<ListViewItem>();
         m_selectedItemList = new List<ListViewItem>();
+        m_scrollRect = m_rectTransform.GetComponentInParent<ScrollRect>();
+        if(m_scrollRect == null)
+            Debug.LogError("ListView can not find ScrollRect");
+
+        GetLayoutAttribute();
+    }
+
+    void Update()
+    {
+        if(m_isBoundsDirty)
+            UpdateBounds();
     }
 
     public void Init(GameObject prefab, Action<int, ListViewItem> refresh, Action<ListViewItem> valueChanged, Action<ListViewItem> clicked)
@@ -76,7 +94,7 @@ public class ListView : MonoBehaviour
         }
 
         itemPrefab = prefab;
-        m_pool = new GameObjectPool(m_transform, itemPrefab);
+        m_pool = new GameObjectPool(m_rectTransform, itemPrefab);
         m_onItemRefresh = refresh;
         m_onItemValueChanged = valueChanged;
         m_onItemValueChanged += OnValueChanged;
@@ -99,13 +117,14 @@ public class ListView : MonoBehaviour
         if (itemPrefab == null) return null;
 
         GameObject go = m_pool.Get();
-        go.transform.SetParent(m_transform);
+        go.transform.SetParent(m_rectTransform);
         go.transform.localPosition = Vector3.zero;
         go.transform.localScale = Vector3.one;
         ListViewItem item = go.GetComponent<ListViewItem>();
         m_itemList.Add(item);
         item.Init(selectType, m_onItemValueChanged, m_onItemClicked);
         go.SetActive(true);
+        SetBoundsDirty();
         return item;
     }
 
@@ -115,6 +134,7 @@ public class ListView : MonoBehaviour
             item.isSelected = false;
         m_pool.Put(item.gameObject);
         m_itemList.Remove(item);
+        SetBoundsDirty();
     }
     
     public void RemoveItem(int index)
@@ -128,6 +148,58 @@ public class ListView : MonoBehaviour
         if(beginIndex > endIndex) return;
         for (int i = beginIndex; i <= endIndex; i++)
             RemoveItem(beginIndex);
+    }
+
+    void SetBoundsDirty()
+    {
+        m_isBoundsDirty = true;
+    }
+
+    void UpdateBounds()
+    {
+        if (m_isHorizontal)
+        {
+            m_columnCount = Mathf.CeilToInt((float)itemCount / m_rowCount);
+            m_rectTransform.sizeDelta = new Vector2(m_itemSize.x * m_columnCount, m_rectTransform.sizeDelta.y);
+        }
+        else
+        {
+            m_rowCount = Mathf.CeilToInt((float)itemCount / m_columnCount);
+            m_rectTransform.sizeDelta = new Vector2(m_rectTransform.sizeDelta.x, m_itemSize.y * m_rowCount);
+        }
+        
+        m_isBoundsDirty = false;
+    }
+
+    void GetLayoutAttribute()
+    {
+        m_initialSize = m_rectTransform.rect.size;
+        m_padding = new Vector2(m_gridLayoutGroup.padding.left + m_gridLayoutGroup.padding.right, m_gridLayoutGroup.padding.top + m_gridLayoutGroup.padding.bottom);
+        m_itemSize = m_gridLayoutGroup.cellSize + m_gridLayoutGroup.spacing;
+        
+        if (m_gridLayoutGroup.constraint == GridLayoutGroup.Constraint.FixedColumnCount)
+        {
+            m_isHorizontal = false;
+            m_columnCount = m_gridLayoutGroup.constraintCount;
+        }
+        else if(m_gridLayoutGroup.constraint == GridLayoutGroup.Constraint.FixedRowCount)
+        {
+            m_isHorizontal = true;
+            m_rowCount = m_gridLayoutGroup.constraintCount;
+        }
+        else
+        {
+            if (m_gridLayoutGroup.startAxis == GridLayoutGroup.Axis.Horizontal)
+            {
+                m_isHorizontal = false;
+                m_columnCount = Mathf.FloorToInt((m_initialSize.x - m_padding.x) / m_itemSize.x);
+            }
+            else
+            {
+                m_isHorizontal = true;
+                m_rowCount = Mathf.FloorToInt((m_initialSize.y - m_padding.y) / m_itemSize.y);
+            }
+        }
     }
     
     void OnValueChanged(ListViewItem item)
